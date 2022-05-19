b420a0b22c0da40a06a669eb7afb4254aa017814
NoahWich <noahwich@buffalo.edu>
2022-05-19 12:25:34 -0400

    Initial Commit

---
+++ Beginner/FirstProgram.cpp
@@ -0,0 +1,11 @@
+// First Program, Ask user for their Favorite Number and then print that number with a message
+#include <iostream>
+
+int main() {
+    int userNum;
+    std::cout << "Enter your Favorite Number [1,100]: ";
+
+    std::cin >> userNum;
+    std::cout << "Amazing, " << userNum << " is my favorite number too!" << std::endl;
+    return 0;
+}
