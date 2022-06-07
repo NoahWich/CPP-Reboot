a422d242658b45b738866c1b47cbdb4509468d2b
NoahWich <noahwich@buffalo.edu>
2022-06-07 17:53:20 -0400

    Added some CPP Files :)

---
+++ Beginner/array.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    int testScores[5] {69, 88, 74, 100, 94};
+
+    cout << "The test score at index 0 is: " << testScores[0];
+    cout << "\nThe test score at index 1 is: " << testScores[1];
+    cout << "\nThe test score at index 2 is: " << testScores[2];
+    cout << "\nThe test score at index 3 is: " << testScores[3];
+    cout << "\nThe test score at index 4 is: " << testScores[4];
+
+    return 0;
+}
